9876c0d22c94e78e7f733bce8afc944c9218d7f1
gregor-fausto <fausto.siegmund@gmail.com>
2023-02-10 10:15:17 -0800

    revise code for seedling survival model checks

--- scripts/004_checkStatisticalModels/13_modelChecksSeedlingSurvivalSupplement.R
+++ scripts/004_checkStatisticalModels/13_modelChecksSeedlingSurvivalSupplement.R
@@ -11,7 +11,7 @@ options(stringsAsFactors = FALSE)
 
 mcmcDirectory = "outputs/002_fitStatisticalModels/mcmcSamples/"
 fullDataDirectory = "outputs/001_prepareDataForModels/"
-outputDirectory = "outputs/004_checkStatisticalModels/"
+outputDirectory = "outputs/004_checkStatisticalModels/01_modelChecksSupplement/"
 
 # - Libraries ----
 library(MCMCvis)
@@ -32,260 +32,18 @@ mcmcSampleDirectory <- paste0(mcmcDirectory,list.files(mcmcDirectory))
 mcmcSamples <- readRDS(mcmcSampleDirectory[[grep("seedlingSurvivalPosteriorSamples.RDS",mcmcSampleDirectory)]])
 
 # - +Read in site names ----
-siteAbiotic <- read.csv("data/siteAbioticData.csv",header=TRUE) 
+siteAbiotic <- read.csv("~/Dropbox/clarkia-demography-projects/data/siteAbioticData.csv",header=TRUE) 
 
 # create variable that arranges populations by easting
 siteIndex <- order(siteAbiotic$easting,decreasing=FALSE)
 siteNames = unique(siteAbiotic$site)[siteIndex]
 
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-# Observations vs. model predictions
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-
-mu0 <- MCMCchains(mcmcSamples,params="mu0")
-mu0.summary <- apply(boot::inv.logit(mu0),2,quantile,c(.025,.5,.975))
-
-mu_year <- MCMCchains(mcmcSamples,params="mu")
-mu.year.summary <- apply(boot::inv.logit(mu_year),2,quantile,c(.025,.5,.975))
-
-tmp.list <- list()
-par(mfrow=c(4,5),mar=c(2,2,1,1))
-for(i in 1:20){
-  
-  plot(NA,xlim=c(0,15),ylim=c(0,1))
-  
-  for(j in 1:15){
-    index= data$site==i&data$year==j
-    tmp=(data$fruitplNumber/data$seedlingNumber)[index]
-    
-    points(rep(j,length(tmp)),tmp,col='gray',pch=1,cex=1)
-    points(j,mean(tmp),pch=4)
-    if(length(mu.year.summary[2,unique(data$siteYearIndex[index])])>0){
-      points(j,mu.year.summary[2,unique(data$siteYearIndex[index])],
-             pch=16,cex=1,col='red')}
-  }
-}
-
-# observed vs predicted with individual level observations
-par(mfrow=c(4,5),mar=c(2,2,1,1))
-for(i in 1:20){ 
-  
-  plot(NA,xlim=c(0,1),ylim=c(0,1))
-  abline(a=0,b=1)
-  
-  for(j in 1:15){
-    index= data$site==i&data$year==j
-    tmp=(data$fruitplNumber/data$seedlingNumber)[index]
-    
-    if(length(mu.year.summary[2,unique(data$siteYearIndex[index])])>0){
-      
-      est.tmp = mu.year.summary[2,unique(data$siteYearIndex[index])]
-      points(x=rep(est.tmp,length(tmp)),y=tmp,col='gray',pch=16,cex=1)
-
-      points(est.tmp,mean(tmp),
-             pch=16,cex=1.25,col='black')
-      }
-  }
-}
-
-par(mfrow=c(4,5),mar=c(2,2,1,1))
-for(i in 1:20){
-  
-  i.tmp = siteIndex[i]
-  
-  plot(NA,xlim=c(0,15),ylim=c(0,1))
-  abline(h=mu0.summary[2,i.tmp],lty='dotted')
-  
-  
-  
-  for(j in 1:15){
-    index= data$site==i.tmp&data$year==j
-    tmp=(data$fruitplNumber/data$seedlingNumber)[index]
-    
-    if(length(mu.year.summary[2,unique(data$siteYearIndex[index])])>0){
-      points(j,mu.year.summary[2,unique(data$siteYearIndex[index])],
-             pch=16,cex=1,col='black')
-      segments(j,y0=mu.year.summary[1,unique(data$siteYearIndex[index])],
-               y1=mu.year.summary[3,unique(data$siteYearIndex[index])])
-    }
-  }
-}
-
-par(mfrow=c(1,1))
-sigma0 <- MCMCchains(mcmcSamples,params="sigma0")
-sigma0.sum<-apply(sigma0,2,quantile,c(0.025,.5,.975))
-plot(siteIndex,sigma0.sum[2,siteIndex],ylim=c(0,3))
-segments(siteIndex,y0=sigma0.sum[1,siteIndex],y1=sigma0.sum[3,siteIndex])
-
-plot(siteAbiotic$easting,sigma0.sum[2,],ylim=c(0,3))
-segments(siteAbiotic$easting,y0=sigma0.sum[1,],y1=sigma0.sum[3,])
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-# Estimates vs. MLEs
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-
-mu0.p  <- MCMCchains(mcmcSamples,params="mu0")
-mu0.p <- apply(boot::inv.logit(mu0.p),2,quantile,c(.025,.5,.975))
-
-mu_year.p  <- MCMCchains(mcmcSamples,params="mu")
-mu.year.p.summary <- apply(boot::inv.logit(mu_year.p),2,quantile,c(.025,.5,.975))
-
-# estimates across all sites
-y=data$fruitplNumber
-n=data$seedlingNumber
-site = data$site
-
-year = data$year
-siteYearIndex = data$siteYearIndex
-d=data.frame(site=site,year=year,siteYearIndex=siteYearIndex,
-             n=n,y=y,p=y/n)
-
-d.summary = d %>% 
-  dplyr::group_by(siteYearIndex) %>% 
-  dplyr::summarise(p.yr=mean(p,na.rm=TRUE),n.yr=sum(n))
-
-df<-d.summary
-
-# observed vs. predicted
-plot(mu.year.p.summary[2,],d.summary$p.yr,xlim=c(0,1),ylim=c(0,1),
-     pch=NA,xlab="Predicted seedling survival (mean of posterior)",
-     ylab="Observed seedling survival (mean of observations)")
-abline(a=0,b=1)
-points(d.summary$p.yr,mu.year.p.summary[2,],pch=1)
-
-par(mfrow=c(4,5),mar=c(4,4,1,1))
-for(i in 1:20){
-  
-  index = data$siteYearIndex[data$site==i]
-  index = unique(index)
-  
-  plot(mu.year.p.summary[2,index],d.summary$p.yr[index],xlim=c(0,1),ylim=c(0,1),
-       pch=NA,xlab="Mean (MLE)",ylab="Posterior median",
-       main=siteNames[i])
-  abline(a=0,b=1)
-  points(mu.year.p.summary[2,index],d.summary$p.yr[index],pch=16)
-  segments(y0=d.summary$p.yr[index],x0=mu.year.p.summary[1,index],x1=mu.year.p.summary[3,index])
-  
-}
-
-mtext(paste0("Predicted seedling survival (mean of posterior)"), side = 1, outer = TRUE, line = 1.5)
-mtext("Observed seedling survival (mean of observations)", side = 2, outer = TRUE, line = 2.2)
-
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-# Graphical Posterior Predictive Checks
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
 
 # ---
-# Graphical checks ----
 # ---
-
-y.sim=MCMCchains(mcmcSamples, params = "fruitplNumber_sim")
-y.obs=data$fruitplNumber
-n.obs=data$seedlingNumber
-n.iter=dim(y.sim)[1]
-subsample = sample(n.iter,1000)
-y.sim.sub = y.sim[subsample,]
-years=2006:2020
-
-pdf(file=paste0(outputDirectory,"ppc-seedlingSurvivalFruiting.pdf"),height=6,width=6)
-
-for(i in 1:length(years)){
-  
-  par(mfrow = c(4,5),
-      oma = c(4,5,0,0) + 0.1,
-      mar = c(1,0,1,1) + 0.1)
-  
-  n.samples = 50
-  iter.ind = sample(1:length(subsample),n.samples)
-  
-  tmpSite = data$site_observed[data$year_observed==i]
-  
-  for(j in 1:20){
-    
-    j.tmp = siteIndex[j]
-    if(j.tmp %in% tmpSite){
-     
-       index=data$site==j.tmp&data$year==i
-      
-      p.obs=y.obs[index]
-      p.sim=y.sim.sub[,index]
-      if(is.matrix(p.sim)){
-        p.sim=p.sim[iter.ind,]
-        list.dens=apply(p.sim,1,density,na.rm=TRUE)
-        all.max.y=max(unlist(lapply(list.dens, "[", "y")))
-        all.max.x=max(unlist(lapply(list.dens, "[", "x")))
-        
-        plot(NA,NA,
-             ylim=c(0,all.max.y),xlim=c(0,all.max.x),
-             xaxt='n',xlab='',ylab='',yaxt='n')
-        for(h in 1:n.samples){
-          m.max=max(p.sim[h,],na.rm=TRUE)
-          m.min=min(p.sim[h,],na.rm=TRUE)
-          
-          dens.x = density(p.sim[h,],from=m.min,to=m.max,na.rm=TRUE)
-          lines(x=dens.x$x,y=dens.x$y,lwd=0.25,col='orange')
-        }
-        
-        p = data$fruitplNumber[index]
-        m.max=max(p,na.rm=TRUE)
-        m.min=min(p,na.rm=TRUE)
-        
-        dens.x = density(p,from=m.min,to=m.max,na.rm=TRUE)
-        
-        lines(x=dens.x$x,y=dens.x$y)
-        
-        if(length(unique(p))==1){ abline(v=unique(p),lty='dotted',col='black')}
-        
-      } else if(is.vector(p.sim)){
-        # lines below are for the case where there is only 1 observation
-        
-        p.sim = p.sim[iter.ind]
-        all.max.x= max(density(p.sim,na.rm=TRUE)$x)
-        all.max.y= max(density(p.sim,na.rm=TRUE)$y)
-        
-        plot(NA,NA,
-             ylim=c(0,all.max.y),xlim=c(0,all.max.x),
-             xaxt='n',xlab='',ylab='',yaxt='n')
-        
-        # plot observed value
-        p = data$fruitplNumber[index]
-        abline(v=p,lty='dotted',col='black')
-        
-        # overlay simulated values
-        tb = table(p.sim)
-        tb.x = as.numeric(names(tb))
-        segments(x0=tb.x,y0=0,y1=tb/sum(tb),col='orange')
-        
-      }
-      
-      # ifelse(i%in%c(1),axis(2L),NA)
-      
-      axis(2,cex=.25,tick=FALSE,line=-1)
-      axis(1,cex=.25,tick=FALSE,line=-1)
-      
-      legend("topright",paste0(siteNames[j],"\n n=",length(p)),bty='n')
-    } else {
-      plot(NA,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
-    }
-  }
-  
-  mtext(paste0("Probability of seedling survival (",years[i],")"), side = 1, outer = TRUE, line = 1.5)
-  mtext("Density", side = 2, outer = TRUE, line = 2.2)
-}
-
-dev.off()
-
-
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
-# Posterior Predictive Checks
-# -------------------------------------------------------------------
-# -------------------------------------------------------------------
+# - Posterior Predictive Checks ---
+# ---
+# ---
 
 # ---
 # Test statistics ----
@@ -293,7 +51,7 @@ dev.off()
 chi2.obs=MCMCchains(mcmcSamples,params=c("chi2.obs"))
 
 n.iter=dim(chi2.obs)[1]
-subsample = sample(n.iter,1000)
+subsample = sample(n.iter,5000)
 
 chi2.obs=MCMCchains(mcmcSamples,params=c("chi2.obs"))[subsample,]
 chi2.sim=MCMCchains(mcmcSamples,params=c("chi2.sim"))[subsample,]
@@ -358,8 +116,9 @@ f=function(y.sim=chains,y.obs=data,n.obs=data2,model.fun=mean){
         tmp.sim=sweep(y.sim[,index], 2, n.obs[index], FUN = '/')
         test.sim=apply(tmp.sim,1,model.fun,na.rm=TRUE)
       } else if(is.vector(tmp.chi2.obs)){
-        tmp.sim=y.sim[index]/n.obs[index]
-        test.sim=model.fun(tmp.sim,na.rm=TRUE)
+        tmp.sim=y.sim[,index]/n.obs[index]
+        test.sim=tmp.sim
+        #test.sim=model.fun(tmp.sim,na.rm=TRUE)
       }
       
       p.test.calc=ifelse(test.sim-test.obs>=0,1,0)
@@ -384,10 +143,10 @@ sims=MCMCchains(mcmcSamples,params=c("fruitplNumber_sim"))[subsample,]
 df=data$fruitplNumber
 df2=data$seedlingNumber
 
-seedlings.min=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=min)
-seedlings.max=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=max)
+# seedlings.min=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=min)
+# seedlings.max=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=max)
 seedlings.mean=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=mean)
-seedlings.sd=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=sd)
+# seedlings.sd=f(y.sim=sims,y.obs=df,n.obs=df2,model.fun=sd)
 
 # convert lists to matrix
 f.convert = function(test.list){
@@ -404,64 +163,208 @@ f.convert = function(test.list){
 
 # return matrix objects
 p.chi.mat<-f.convert(p.chi.list)
-seedlings.min.mat<-f.convert(seedlings.min)
-seedlings.max.mat<-f.convert(seedlings.max)
+# seedlings.min.mat<-f.convert(seedlings.min)
+# seedlings.max.mat<-f.convert(seedlings.max)
 seedlings.mean.mat<-f.convert(seedlings.mean)
-seedlings.sd.mat<-f.convert(seedlings.sd)
+# seedlings.sd.mat<-f.convert(seedlings.sd)
 
 colfunc <- colorRampPalette(c("white", "black"))
 col.vec=colfunc(15)
 
-pdf(file=paste0(outputDirectory,"pvals-seedlingSurvivalFruiting.pdf"),height=6,width=6)
 
-par(mfrow=c(1,1))
-time.sample = 1:length(years)
-plot(NA,NA,
-     ylim=c(0,1),pch=16,xlim=c(-.5,24.5),
-     xlab="",ylab="",
-     main="Seedling survival to fruiting",type='n',
-     xaxt='n',yaxt='n',frame=FALSE)
 
-start = c(0,5,10,15,20)
-offset = seq(0,4,length.out=length(years))
-polygon(x=c(4.5,4.5,9.5,9.5),y=c(-1,2,2,-1),col='gray90',border=0)
-polygon(x=c(14.5,14.5,19.5,19.5),y=c(-1,2,2,-1),col='gray90',border=0)
+f.plot = function(diagnostic.test.mat,years){
+  
+  plot(NA,NA,
+       ylim=c(0,1),pch=16,xlim=c(min(years),max(years)),
+       xlab="",ylab="",
+       main=NULL,type='n',
+       xaxt='n',yaxt='n',frame=FALSE)
+  
+  start = years[1]
+  offset = seq(0,length(years),by=1)
+  
+  for(j in 1:length(years)){
+    
+    points(x=rep(start+offset[j],20)+rnorm(20,0,.05),
+           pch=21,cex=.8,
+           bg=ifelse(diagnostic.test.mat[,j]>0.95|
+                       diagnostic.test.mat[,j]<0.05,'orange','gray95'),
+           y=diagnostic.test.mat[,j])
+    
+  }
+  
+  abline(h=.05,lty='dotted')
+  abline(h=.95,lty='dotted')
+  box(which="plot",bty="l",col='black')
+  
+  axis(1, seq(min(years),max(years),by=2),
+       labels = seq(min(years),max(years),by=2), 
+       las = 1,
+       col = NA, col.ticks = 1, cex.axis = 1)
+  axis(2, seq(0,1,by=.2),
+       seq(0,1,by=.2), las = 1,
+       col = NA, col.ticks = 1, cex.axis = 1)
+}
+
+
+pdf(file=paste0(outputDirectory,"pvals-seedlingSurvivalFruiting.pdf"),height=4,width=6)
+
+par(mfrow = c(1,2),
+    oma = c(2,2.5,0,0) + 0.1,
+    mar = c(0,.5,1,1) + 0.1,
+    mgp=c(3,.5,0))
+
+f.plot(diagnostic.test.mat = seedlings.mean.mat,years=2006:2020)
+mtext("Bayesian p-value", side=2, line=2, cex.lab=1,las=0, col="black")
+title("A. Mean", adj=0)
+
+f.plot(diagnostic.test.mat = p.chi.mat,years=2006:2020)
 box(which="plot",bty="l",col='black')
-abline(h=0.5,lty='dotted')
-
-f.box=function(x,y,width=.2){
-  d=boxplot(y,plot=FALSE)
-  segments(x0=x,y0=d$stats[1],y1=d$stats[5],lty='solid')
-  rect(xleft=x-width/2,xright=x+width/2,
-       ybottom=d$stats[2],ytop=d$stats[4],col='white')
-  segments(x0=x-width/2,x1=x+width/2,
-           y0=d$stats[3],lwd=2)
-  
-}
-
-for(j in 1:length(years)){
-  
-  f.box(x=start[1]+offset[j],y=seedlings.min.mat[,j],width=.2)
-  
-  f.box(x=start[2]+offset[j],y=seedlings.max.mat[,j],width=.2)
-  
-  f.box(x=start[3]+offset[j],y=seedlings.mean.mat[,j],width=.2)
-  
-  f.box(x=start[4]+offset[j],y=seedlings.sd.mat[,j],width=.2)
-  
-  f.box(x=start[5]+offset[j],y=p.chi.mat[,j],width=.2)
-  
-}
-
-
-axis(1, c(2,7,12,17,22),
-     labels = c("min","max","mean","sd","Chi-2"), las = 1, 
-     col = NA, col.ticks = 1, cex.axis = 1)
-axis(2, seq(0,1,by=.2),
-     seq(0,1,by=.2), las = 1, 
-     col = NA, col.ticks = 1, cex.axis = 1)
+title("B. Chi-squared",adj=0)
 
 dev.off()
+# 
+# par(mfrow=c(4,5),mar=c(1,1,1,1),oma=c(1,1,1,1))
+# for(i in 1:20){
+#   plot(1:15,seedlings.mean.mat[i,],type='b',ylim=c(0,1),
+#        pch=ifelse(seedlings.mean.mat[i,]<.05|seedlings.mean.mat[i,]>.95,21,16))
+# }
+# 
+# par(mfrow=c(3,5),mar=c(1,1,1,1),oma=c(1,1,1,1))
+# for(i in 1:15){
+#   plot(1:20,seedlings.mean.mat[,i][siteIndex],type='b',ylim=c(0,1),
+#        pch=ifelse(seedlings.mean.mat[,i][siteIndex]<.05|seedlings.mean.mat[,i][siteIndex]>.95,21,16))
+# }
+# 
+# var.mat = matrix(NA,nrow=20,ncol=15)
+# for(i in 1:20){
+#   for(j in 1:15){
+#     index=data$site==i&data$year==j
+#     n_sdlg=data$seedlingNumber[index]
+#     n_frts=data$fruitplNumber[index]
+#     #p=n_frts/n_sdlg
+#     var.mat[i,j]=var(p,na.rm=TRUE)
+#     var.mat[i,j]=sum(n_sdlg,na.rm=TRUE)
+#   }
+# }
+# plot(var.mat,seedlings.mean.mat)
+# 
+# par(mfrow=c(4,5),mar=c(1,1,1,1),oma=c(1,1,1,1))
+# for(i in 1:20){
+#   plot(var.mat[i,],seedlings.mean.mat[i,],type='p',ylim=c(0,1),
+#        pch=ifelse(seedlings.mean.mat[i,]<.05|seedlings.mean.mat[i,]>.95,21,16))
+# }
+
+
+# ---
+# Graphical checks ----
+# ---
+
+
+y.sim=MCMCchains(mcmcSamples, params = "fruitplNumber_sim")
+y.obs=data$fruitplNumber
+n.obs=data$seedlingNumber
+years=2006:2020
+n.samples = 50
+n.sim=5000
+
+pdf(file=paste0(outputDirectory,"ppc-seedlingSurvivalFruiting.pdf"),height=6,width=6)
+
+par(mfrow = c(3,5),
+    oma = c(2,2.5,0,0) + 0.1,
+    mar = c(0,.5,1,1) + 0.1,
+    mgp=c(2,1,0))
+  
+for(j in 1){
+    
+  for(i in 1:length(years)){
+    
+    iter.ind = sample(1:n.sim,n.samples)
+    tmpSite = data$site_observed[data$year_observed==i]
+    
+    j.tmp = siteIndex[j]
+    if(j.tmp %in% tmpSite){
+      
+      index=data$site==j.tmp&data$year==i
+      
+      p.obs=y.obs[index]/n.obs[index]
+     # p.obs=sweep(y.obs[index], 2, n.obs[index], FUN = '/')
+      
+  #    p.sim=as.matrix(y.sim[,index])
+      p.sim=sweep(t(y.sim[,index]), 2, n.obs[index], FUN = '/')
+      p.sim=as.matrix(t(p.sim)[iter.ind,])
+      
+      if(is.matrix(p.sim)&dim(p.sim)[2]>1){
+        list.dens=apply(p.sim,1,density,na.rm=TRUE)
+        all.max.y=max(unlist(lapply(list.dens, "[", "y")))
+        all.max.x=max(unlist(lapply(list.dens, "[", "x")))
+        
+        plot(NA,NA,
+             ylim=c(0,all.max.y),xlim=c(0,all.max.x),
+             xaxt='n',xlab='',ylab='',yaxt='n')
+        for(h in 1:n.samples){
+          m.max=max(p.sim[h,],na.rm=TRUE)
+          m.min=min(p.sim[h,],na.rm=TRUE)
+          
+          dens.x = density(p.sim[h,],from=m.min,to=m.max,na.rm=TRUE)
+          lines(x=dens.x$x,y=dens.x$y,lwd=0.25,
+                col=ifelse(p.chi.mat[j,i]>.95|p.chi.mat[j,i]<.05,
+                             'orange','gray75'))
+        }
+        
+        p = data$fruitplNumber[index]
+        m.max=max(p,na.rm=TRUE)
+        m.min=min(p,na.rm=TRUE)
+        
+        dens.x = density(p,from=m.min,to=m.max,na.rm=TRUE)
+        
+        lines(x=dens.x$x,y=dens.x$y)
+        
+        if(length(unique(p))==1){ abline(v=unique(p),lty='dotted',col='black')}
+        
+      } else if(is.matrix(p.sim)&dim(p.sim)[2]==1){
+        # lines below are for the case where there is only 1 observation
+        
+        all.max.x= max(density(p.sim,na.rm=TRUE)$x)
+        all.max.y= max(density(p.sim,na.rm=TRUE)$y)
+        
+        plot(NA,NA,
+             ylim=c(0,all.max.y),xlim=c(0,all.max.x),
+             xaxt='n',xlab='',ylab='',yaxt='n')
+        
+        # plot observed value
+        p = data$fruitplNumber[index]
+        abline(v=p,lty='dotted',col='black')
+        
+        # overlay simulated values
+        tb = table(p.sim)
+        tb.x = as.numeric(names(tb))
+        segments(x0=tb.x,y0=0,y1=tb/sum(tb),col='orange')
+        
+      }
+      
+      ifelse(i%in%c(1),axis(2L),NA)
+      
+      axis(2,cex=.25,tick=FALSE,line=-1)
+      axis(1,cex=.25,tick=FALSE,line=-1)
+      
+      legend("topright"
+             ,paste0(siteNames[j],": ",years[i],"\n n=",length(p)),
+             bty='n')
+    } else {
+      plot(NA,xlim=c(0,1),ylim=c(0,1),axes=FALSE)
+    }
+  }
+  
+  mtext(paste0("Probability of seedling survival"), 
+        side = 1, outer = TRUE, line = .75)
+  mtext("Density", side = 2, outer = TRUE, line = 1.2)
+}
+
+dev.off()
+
+
 
 # -------
 # Chi-2 ----
@@ -706,4 +609,14 @@ mtext("Bayesian p-value (chi-2)", side = 2, outer = TRUE, line = 2.2)
 
 
 dev.off()
+
+
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+# Graphical Posterior Predictive Checks
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+
 
