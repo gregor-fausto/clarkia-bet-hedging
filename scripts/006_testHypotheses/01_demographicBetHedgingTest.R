72c4ffe57cb929facc08bb9e47beabb8aa30b151
gregor-fausto <fausto.siegmund@gmail.com>
2023-06-11 17:36:14 -0700

    For Figure 3, arrange the panels vertically or as an inverted triangle to help production lay out the figure; increase font sizes.

--- scripts/006_testHypotheses/01_demographicBetHedgingTest.R
+++ scripts/006_testHypotheses/01_demographicBetHedgingTest.R
@@ -146,7 +146,9 @@ var.lambda.nosb.mean = apply(var.lambda.nosb.mat,2,mean)
 # - Manuscript figure  ----
 
 # - +set font sizes ----
+pt14 = 14/12
 pt12 = 1
+pt11 = 11/12
 pt10 = 10/12
 pt9 = 9/12
 pt8 = 8/12
@@ -164,12 +166,18 @@ demographicBetHedgingTestData = data.frame(site=siteNames,
 outputDirectory <- "outputs/006_hypothesisTesting/"
 saveRDS(demographicBetHedgingTestData,paste0(outputDirectory,"demographicBetHedgingTest.RDS"))
 
+demographicBetHedgingTestData<-readRDS(paste0(outputDirectory,"demographicBetHedgingTest.RDS"))
+siteNames=demographicBetHedgingTestData$site;
+lambda.a=demographicBetHedgingTestData$lambda.a;lambda.a.nosb=demographicBetHedgingTestData$lambda.a.nosb;
+var.lambda.mean=demographicBetHedgingTestData$var.lambda;var.lambda.nosb.mean=demographicBetHedgingTestData$var.lambda.nosb;
+lambda.mean=demographicBetHedgingTestData$lambda;lambda.nosb.mean=demographicBetHedgingTestData$lambda.nosb
+
 # - Plot Figure 3  ----
 
-tiff(filename=paste0("products/figures/betHedgingTest.tif"),
-     height=2.5,width=6.5,units="in",res=800,compression="lzw",pointsize=12)
+tiff(filename=paste0("products/figures/betHedgingTest-finalRevision.tif"),
+     height=6.5,width=2.5,units="in",res=800,compression="lzw",pointsize=12)
 
-par(mfrow=c(1,3),mar=c(0,3,0,0)+.1,oma=c(2.5,0,.8,0)+.1,mgp=c(3,.45,0))
+par(mfrow=c(3,1),mar=c(3,3.75,0,0)+.1,oma=c(0,0,1,0)+.1,mgp=c(3,.45,0))
 
 # - +PANEL A  ----
 plot(lambda.a,lambda.a.nosb,
@@ -187,22 +195,22 @@ points(lambda.a,lambda.a.nosb,lwd = 0.5,
        pch=21,col='black',cex=1,
        bg=rgb(red = 1, green = 1, blue = 1, alpha = 0.5))
 
-axis(1, seq(0,10,by=2), padj = -.5,
+axis(1, seq(0,10,by=2), padj = 0, tcl = -0.4,
      labels = seq(0,10,by=2), line = 0,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(1, seq(1,9,by=2),labels=FALSE)
-axis(2, seq(0,40,by=10),
-     labels = seq(0,40,by=10), las = 1, line = 0, hadj= 1.2,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(2, seq(5,35,10),labels=FALSE)
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(1, seq(1,9,by=2),labels=FALSE,tck=-0.02)
+axis(2, seq(0,40,by=10), tcl = -0.4,
+     labels = seq(0,40,by=10), las = 1, line = 0, hadj= 1.1,
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(2, seq(5,35,10),labels=FALSE,tck=-0.02)
 
 mtext(expression(lambda[a] ~ 'without seedbank'),
-      side=2,line=1.5,adj=.5,col='black',cex=pt8)
+      side=2,line=2,adj=.5,col='black',cex=pt11)
 mtext(expression(lambda[a] ~ 'with seedbank'),
-      side=1,line=1.5,col='black',cex=pt8,adj=.5)
+      side=1,line=1.75,col='black',cex=pt11,adj=.5)
 
 abline(a=0,b=1,lty='dotted')
-mtext("A.", adj = 0, cex=pt10)
+mtext("A.", adj = 0, cex=pt12)
 
 # - +PANEL B  ----
 plot(var.lambda.mean,var.lambda.nosb.mean,
@@ -220,31 +228,31 @@ points(var.lambda.mean,var.lambda.nosb.mean,
        pch=21,col='black',cex=1,lwd = 0.5,
        bg=rgb(red = 1, green = 1, blue = 1, alpha = 0.5))
 
-axis(1, seq(0,60,by=10), padj = -.5,
+axis(1, seq(0,60,by=10), padj = 0, tcl = -0.4,
      labels = seq(0,60,by=10), line = 0,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(1, seq(5,55,by=10),labels=FALSE)
-axis(2, seq(0,3000,by=500),
-     labels = seq(0,3000,by=500), las = 1, line = 0, hadj= 1.2,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(2, seq(250,2750,500),labels=FALSE)
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(1, seq(5,55,by=10),labels=FALSE,tck=-0.02)
+axis(2, seq(0,3000,by=500), tcl = -0.4,
+     labels = seq(0,3000,by=500), las = 1, line = 0, hadj= 1.1,
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(2, seq(250,2750,500),labels=FALSE,tck=-0.02)
 
 mtext(expression(Var(lambda) ~ 'without seedbank'),
-      side=2,line=1.75,adj=.5,col='black',cex=pt8)
+      side=2,line=2.25,adj=.5,col='black',cex=pt11)
 mtext(expression(Var(lambda) ~ 'with seedbank'),
-      side=1,line=1.5,col='black',cex=pt8,adj=.5)
+      side=1,line=1.75,col='black',cex=pt11,adj=.5)
 
 abline(a=0,b=1,lty='dotted')
-mtext("B.", adj = 0, cex=pt10)
+mtext("B.", adj = 0, cex=pt12)
 
 # - +PANEL C  ----
 plot(lambda.mean,lambda.nosb.mean,
      type='n',
-     xlim=c(0,4),ylim=c(0,19),
+     xlim=c(0,4),ylim=c(0,20),
      xlab = "",
      ylab = "",
      xaxt= "n", yaxt="n",
-     cex.lab = pt10, cex.axis = pt8)
+     cex.lab = pt10, cex.axis = pt12)
 abline(a=0,b=1,lty='dotted')
 
 
@@ -259,21 +267,21 @@ points(lambda.mean,lambda.nosb.mean,
        pch=21,col='black',cex=1, lwd = 0.5,
        bg=rgb(red = 1, green = 1, blue = 1, alpha = 0.5))
 
-axis(1, seq(0,4,by=1), padj = -.5,
+axis(1, seq(0,4,by=1), padj = 0, tcl = -0.4,
      labels = seq(0,4,by=1), line = 0,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(1, seq(.5,3.5,by=1),labels=FALSE)
-axis(2, seq(0,18,by=2),
-     labels = seq(0,18,by=2), las = 1, line = 0, hadj= 1.2,
-     col = NA, col.ticks = 1, cex.axis = pt8)
-axis(2, seq(1,19,2),labels=FALSE)
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(1, seq(.5,3.5,by=1),labels=FALSE,tck=-0.02)
+axis(2, seq(0,20,by=4),tcl = -0.4,
+     labels = seq(0,20,by=4), las = 1, line = 0, hadj= 1.1,
+     col = NA, col.ticks = 1, cex.axis = pt12)
+axis(2, seq(2,20,4),labels=FALSE,tck = -.02)
 
 mtext(expression(lambda[s] ~ 'without seedbank'),
-      side=2,line=1.5,adj=.5,col='black',cex=pt8)
+      side=2,line=2,adj=.5,col='black',cex=pt11)
 mtext(expression(lambda[s] ~ 'with seedbank'),
-      side=1,line=1.5,col='black',cex=pt8,adj=.5)
+      side=1,line=1.5,col='black',cex=pt11,adj=.5)
 
-mtext("C.", adj = 0, cex=pt10)
+mtext("C.", adj = 0, cex=pt12)
 
 dev.off()
 
